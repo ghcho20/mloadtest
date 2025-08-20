774d08d12294316610d142a4912320686c780f3e
gcho <ghcho20@gmail.com>
2025-08-20 09:58:45 +0900

    2 test users for each query - FindUser to preloaded oltp collection - InsertUser to oldtwrite collection

--- mlocust/oltp_test.py
+++ mlocust/oltp_test.py
@@ -13,18 +13,16 @@ def init_config():
     """
     doc_size = int(DEFAULTS['DOC_SIZE'])
     docs_per_batch = int(DEFAULTS['DOCS_PER_BATCH'])
-    rweight = int(DEFAULTS['FIND_WEIGHT'])
-    wweight = int(DEFAULTS['BULK_INSERT_WEIGHT'])
 
-    print(f'Config: doc_size={doc_size}, docs_per_batch={docs_per_batch}, rweight={rweight}, wweight={wweight}')
-    return (doc_size, docs_per_batch, rweight, wweight)
+    print(f'Config: doc_size={doc_size}, docs_per_batch={docs_per_batch}')
+    return (doc_size, docs_per_batch)
 
-class TestUser(MongoUser):
+class InsertUser(MongoUser):
     """
     User class for load testing MongoDB operations.
     """
     wait_time = between(0.0, 0.0)
-    doc_size, docs_per_batch, rweight, wweight = init_config()
+    doc_size, docs_per_batch = init_config()
 
 
     def __init__(self, environment):
@@ -38,14 +36,29 @@ class TestUser(MongoUser):
         #     self.db.drop_collection(DEFAULTS['COLLECTION_NAME'])
         self.coll, _ = self.ensure_collection(DEFAULTS['COLLECTION_NAME'])
 
-    @mongodb_task(weight=rweight)
-    def find(self):
-        filters = random.sample(range(1, 65_536), 2)
-        docs = list(self.coll.find({'filter': {'$in': filters}}))
-        return 4*1024*len(docs) if docs else 0
-
-    @mongodb_task(weight=wweight)
+    @mongodb_task(weight=1)
     def bulk_insert(self):
         docs_batch = gen_docs(self.doc_size, self.docs_per_batch)
         self.coll.insert_many(docs_batch)
         return len(bson.BSON.encode(docs_batch[0])) * self.docs_per_batch
+
+class FindUser(MongoUser):
+    """
+    User class for load testing MongoDB find operations.
+    """
+    wait_time = between(0.0, 0.0)
+
+    def __init__(self, environment):
+        super().__init__(environment)
+
+    def on_start(self):
+        """
+        Initialize the user by connecting to the MongoDB cluster.
+        """
+        self.coll, _ = self.ensure_collection(DEFAULTS['COLLECTION_OLTP'])
+
+    @mongodb_task(weight=1)
+    def find(self):
+        filters = random.sample(range(1, 65_536), 5)
+        docs = list(self.coll.find({'filter': {'$in': filters}}))
+        return 4*1024*len(docs) if docs else 0
